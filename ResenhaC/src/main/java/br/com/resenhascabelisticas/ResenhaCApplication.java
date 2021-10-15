ab3d52802d8c25726a1be666e6ccc4da8004e409
Joelma <joohtrader@gmail.com>
2021-10-15 19:44:42 -0300

    implementacao da model,controller e repository

---
+++ ResenhaC/src/main/java/br/com/resenhascabelisticas/ResenhaCApplication.java
@@ -0,0 +1,13 @@
+package br.com.resenhascabelisticas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResenhaCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResenhaCApplication.class, args);
+	}
+
+}
